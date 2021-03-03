3843489c875db5dc3c2940fcd8c18fb9d53ff52b
Davidthesoul <davidthesoul@hotmail.com>
2021-03-03 17:24:16 -0300

    Conexion con Bd

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ UTN - Programación Web Full Stack
 UNIDAD 3 TRABAJO PRÁCTICO 4 
 David Graterol   */
 
-"use strict"
 const express = require('express');
 const mysql = require("mysql");
 const util = require ("util");
-//const PATH = require('path');
+const PATH = require('path');
 const app = express();
+var bodyParser = require('body-parser');
 const port = 3000;
+const http = require('http');
 app.use(express.json());  //permite el mapeo de la peticion  json a objeto js
-
-
 //conexión con mysql
+app.use(express.static(__dirname + '/public' ));
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(express.static(path.join(__dirname,'./public')));
+// a manera de prueba uso el app.route para el metodo GET y coloco directo en Localhost
+app.route('/')
+
+.get(function (req, res) {
+    res.sendFile(PATH.join(__dirname+"/formulario.html"));
+  });
 
 const conexion = mysql.createConnection({   //string de conexión
   host: "localhost",
   user: "root",
   password: "",
-  database: "listacompras"
+  database: "utn"
 });
 conexion.connect((error)=>{
 if (error){
@@ -37,77 +45,59 @@ app.listen(port, ()=> {
 const qy= util.promisify(conexion.query).bind(conexion); 
 //permite el uo de asyn-await en la conexion mysql
 
-
-/**
- * Categorias de productos
- * GET para deolver todas las categorias
- * GET id para devolver uno
- * POST guardar una categoria
- * PUT para modificar una categoria existente
- * DELETE para borrar una categoria
- * 
- * ruta -> /categoria
- */
-//codigo para solicitar ruta
-app.get("/categoria", async(req, res) => {
+//metodo GET para consultar toda la tabla de alumnos
+app.get("/alumnos", async(req, res) => {
 try {
-  const query = "SELECT * FROM categoria";
-  
+  const query = "SELECT * FROM alumnos";
   const respuesta =await qy(query);
-
   res.send({"respuesta": respuesta});
-
-
 }
 catch(e){
   console.error(e.message);
   res.status(413).send({"error": e.message});
   
-}
-
-});
-
-app.get("/categoria/:id", async(req, res) => {
+}});
+//metodo get para consultar 1 alumno
+app.get("/alumnos/:id", async(req, res) => {
   try {
-    const query = "SELECT * FROM categoria WHERE id = ?";
-    
+    const query = "SELECT * FROM alumnos WHERE id = ?";
     const respuesta =await qy(query, [req.params.id]);
     console.log(respuesta);
     res.send({"respuesta": respuesta});
-  
-  
   }
   catch(e){
     console.error(e.message);
     res.status(413).send({"error": e.message});
-    
   }
 });
 
-app.post("/categoria", async(req, res) => {
+
+//metodo POST para agregar un nuevo alumno
+app.post("/alumnos", async(req, res) => {
   try {
     //valido que me mande correctamente la información
    if (!req.body.nombre){
      throw new Error("falta enviar el nombre");
    }
-
    const nombre = req.body.nombre;
-  //verifico que no existe previamente esa categoria
- //toUppercase permite indiscrimiar mayusculas o minusculas
+   const apellido = req.body.apellido;
+   const edad = req.body.edad;
+   const telefono = req.body.telefono;
+   const dni = req.body.dni;
+   const pais = req.body.pais;
+   const fechanac = req.body.fechanac;
 
-   let query = "SELECT id FROM categoria WHERE nombre = ?";
-   
+   let query = "SELECT * FROM `alumnos`"; 
    let respuesta = await qy(query, [nombre]);
-
    if (respuesta.leng > 0){
-    throw new Error("esa categoria ya existe");
+    throw new Error("ese nombre ya existe");
    }
 
-   //guardo nueva categoria
+   //guardo nuevo alumno en base de datos
 
-  query ="INSERT INTO categoria (nombre) VALUE (?)";
-  respuesta = await qy(query, [nombre]); 
-   //console.log(respuesta);
+  query ="INSERT INTO alumnos (nombre, apellido, edad, telefono, dni, pais, fechanac)  VALUE (?,?,?,?,?,?,?)";
+  respuesta = await qy(query, [nombre, apellido, edad, telefono, dni, pais, fechanac]); 
+  console.log(respuesta);
   res.send({"respuesta":respuesta.insertId});
   }
   catch(e){
@@ -118,25 +108,60 @@ app.post("/categoria", async(req, res) => {
 });
 
 
-app.put("/categoria/:id", async (req, res) =>{
+//metodo POST para agregar un nuevo alumno
+app.post("/formulario", async(req, res) => {
+  try {
+    //valido que me mande correctamente la información
+   if (!req.body.nombre){
+     throw new Error("falta enviar el nombre");
+   }
+   const nombre = req.body.nombre;
+   const apellido = req.body.apellido;
+   const edad = req.body.edad;
+   const telefono = req.body.telefono;
+   const dni = req.body.dni;
+   const pais = req.body.pais;
+   const fechanac = req.body.fechanac;
+
+   let query = "SELECT * FROM `alumnos`"; 
+   let respuesta = await qy(query, [nombre]);
+   if (respuesta.leng > 0){
+    throw new Error("ese nombre ya existe");
+   }
+
+   //guardo nuevo alumno en base de datos
+
+  query ="INSERT INTO alumnos (nombre, apellido, edad, telefono, dni, pais, fechanac)  VALUE (?,?,?,?,?,?,?)";
+  respuesta = await qy(query, [nombre, apellido, edad, telefono, dni, pais, fechanac]); 
+  console.log(respuesta);
+  res.send({"respuesta":respuesta.insertId});
+  }
+  catch(e){
+    console.error(e.message);
+    res.status(413).send({"error": e.message});
+    
+  }
+});
+
+
+
+app.put("/alumnos/:id", async (req, res) =>{
   try {
     if (!req.body.nombre){
       throw new Error ("No enviaste el nombre");
     }
     
-    let query = "SELECT * FROM categoria WHERE nombre = ? AND id <> ?";
-
+    let query = "SELECT * FROM alumnos WHERE nombre = ? AND id <> ?";
     let respuesta = await qy(query, [req.body.nombre, req.params.id]);
    // console.log("respuesta: ",respuesta);
     if (respuesta.length > 0) {
-      throw new Error ("El nombre de la categoria que quieres poner ahora ya existe");
+      throw new Error ("El nombre del alumno que quieres poner ahora ya existe");
     }
 
-    query = "UPDATE categoria SET nombre = ? WHERE id = ?";
-
+    query = "UPDATE alumnos SET nombre = ? WHERE id = ?";
     respuesta = await qy(query, [req.body.nombre, req.params.id]);
-      //console.log(respuesta);
-     res.send({"respuesta": respuesta.affectedRows});
+    //console.log(respuesta);
+    res.send({"respuesta": respuesta.affectedRows});
   }
   catch(e){
     console.error(e.message);
@@ -147,20 +172,16 @@ app.put("/categoria/:id", async (req, res) =>{
 });
 
 
-app.delete("/categoria/:id", async (req, res) => {
+app.delete("/alumnos/:id", async (req, res) => {
 try{
-  let query = "SELECT * FROM producto WHERE id_categoria = ?";
-
+  let query = "SELECT * FROM alumnos WHERE id = ?";
   let respuesta = await  qy(query, [req.params.id]);
-
   if (respuesta.length > 0){
     throw new Error ("Esta categoria tiene productos asociados, no se puede borrar");
   }
 
-  query = "DELETE FROM categoria WHERE id = ?";
-
+  query = "DELETE FROM alumnos WHERE id = ?";
   respuesta = await qy(query, [req.params.id]);
-
   res.send({"respuesta": respuesta.affectedRows});
 
 }
@@ -172,57 +193,4 @@ catch(e){
 
 });
 
-
-/**
- * Produtos
- * 
- * Ruta -> /producto
- */
-
-app.post("/producto", async (req, res) => {
-  try{
-      if (!req.body.nombre || !req.body.id_categoria){
-        throw new Error("No enviaste los datos obligatorios que son nombre y categoria");
-      }
-
-      let query = "SELECT * FROM categoria WHERE id = ?";
-      let respuesta = await qy(query, [req.body.id_categoria]);
-
-      if (respuesta.length == 0){
-        throw new Error ("Esa Categoria no existe");
-      }
-
-      query = "SELECT * FROM producto WHERE nombre = ?";
-      respuesta = await  qy(query, [req.body.nombre]);
-
-      if (respuesta.length > 0){
-        throw new Error ("Ese nombre ya existe");
-      }
-
-      let descripcion = "";
-      if (req.body.descripcion){
-        descripcion = req.body.descripcion;
-      }
-
-      query = "INSERT INTO producto (nombre, descripcion, id_categoria) VALUES (?,?,?)";
-      console.log(respuesta);
-      respuesta = await qy(query, [req.body.nombre, descripcion, req.body.id_categoria]);
-      res.send({"respuesta":respuesta.insertId});
-
-  }
-
-
-  catch(e){
-    console.error(e.message);
-    res.status(413).send({"Error": e.message});
-    
-  }
-
-});
-
- /**
-  * Lista de compras 
-  * 
-  * Ruta -> /lista
-  */
 
